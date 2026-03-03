dd351ee5b884efbfc8f905adaeb6a30a14cd693d
Rabi <myEmail@example.com>
2026-03-03 09:20:51 +0530

    cloudinary upload process from local storage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,69 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if(!localFilePath) return null;
+    const uploadResponse = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+
+    console.log("File is uploaded succesfully on cloudinary.... Path => ",localFilePath )
+    console.log("Upload Response URL => ", uploadResponse.url)
+    return uploadResponse;
+  } catch (error) {
+    fs.unlinkSync(localFilePath)//If file upload on cloudinary faild then removed the file from our local storage
+    return null;
+  }
+}
+
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async function () {
+//   // Configuration
+
+//   // Upload an image
+
+//   // Optimize delivery by resizing and applying auto-format and auto-quality
+//   const optimizeUrl = cloudinary.url('shoes', {
+//     fetch_format: 'auto',
+//     quality: 'auto'
+//   });
+
+//   console.log(optimizeUrl);
+
+//   // Transform the image: auto-crop to square aspect_ratio
+//   const autoCropUrl = cloudinary.url('shoes', {
+//     crop: 'auto',
+//     gravity: 'auto',
+//     width: 500,
+//     height: 500,
+//   });
+
+//   console.log(autoCropUrl);
+// })();

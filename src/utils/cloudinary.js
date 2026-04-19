0f3d144ce31b50a2eeaefa9b0e2186714fca9c4d
Rabi <myEmail@example.com>
2026-04-20 02:39:09 +0530

    add delete method for cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -26,7 +26,38 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 }
 
-export {uploadOnCloudinary}
+/*
+Cloudinary URL format: 
+https://res.cloudinary.com/cloud-first/<resource_type>/<type or delevery type>/v<version>/<public_id>.<extension>
+EX: https://res.cloudinary.com/cloud-first/image/upload/v1776620753/nakpv0jnbp82cddlzyh4.png
+
+Syntax: cloudinary.uploader.destroy(publicId, options).
+
+publicId (string): the asset’s public ID. For images and videos, do not include the file extension. For raw files, include the extension.
+options (object): optional settings.
+• resource_type (string): image, raw, or video (default image).
+• type (string): delivery type (default upload).
+• notification_url (string): webhook URL to notify when the delete completes.
+• invalidate (boolean): if true, invalidates CDN cached copies (default false).
+
+Return: on success, the promise resolves to { result: "ok" }.
+Note: for deleting a video from cloudinary you have to pass 'resource_type'.
+*/
+
+const deleteFromCloudinary = async (url) => {
+  try {
+    if(!url) return null;
+    const regex = /\/([^\/]+)\/upload\/v\d+\/(.+)\.[a-zA-Z]+$/
+    const match = url.match(regex); // match[1] = resource_type. match[2] = public_id
+
+    const deletedResponse = await cloudinary.uploader.destroy(match[2], {resource_type: match[1], invalidate: true})
+    return deletedResponse;
+  } catch (error) {
+    console.log("Cloudinary deletation faild. Error: ",error.message)
+    return error.message;
+  }
+}
+export {uploadOnCloudinary, deleteFromCloudinary}
 
 
 
